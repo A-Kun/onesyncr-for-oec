27bf183fa56b7b3a3c3bb752e81958b0b7ac24e8
poppintk <youdonghoho@hotmail.com>
2016-09-16 21:30:22 -0400

    layout web established

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
-console.log('This would be the main JS file.');
+window.onload = function(){
+	// get all li tags
+	var lis = document.getElementsByTagName('li');
+
+	// get target element
+	var target = document.getElementById('main_content_wrap');
+
+	// looping through all  li tags listen click event
+	for(var i = 0; i<lis.length; i++){
+		lis[i].onclick = function(){
+			startShow(this);
+		}
+	}
+
+}
+
+function startShow(obj){
+	alert(this.name);
+	console.log(this.name);
+
+}
