7012521cd51d042fd21509ff4035abb02a4f4e81
Andrew Wang <andrewpy.wang@mail.utoronto.ca>
2016-10-03 01:33:56 -0400

    Add Deliverable #2 page

---
+++ javascripts/home.js
@@ -0,0 +1,3 @@
+function goToDeliverable() {
+	document.body.innerHTML = '<object type="text/html" data="d2.html"></object>';
+}
--- javascripts/main.js
+++ javascripts/main.js
@@ -2,15 +2,12 @@ window.onload = function(){
 	// get all li tags
 	var lis = document.getElementsByTagName('li');
 
-
-
 	// looping through all  li tags listen click event
 	for(var i = 0; i<lis.length; i++){
 		lis[i].onclick = function(){
 			startShow(this);
 		}
 	}
-
 }
 
 function startShow(obj){
@@ -22,6 +19,7 @@ function startShow(obj){
 		target.innerHTML='<object type="text/html" data="contact.html" ></object>';
 	}else if(obj.innerHTML == "News"){
 		target.innerHTML='<object type="text/html" data="news.html" ></object>';
-	} 
-
+	}else if(obj.innerHTML == "Deliverable #2"){
+		target.innerHTML='<object type="text/html" data="d2.html" ></object>';
+	}
 }
