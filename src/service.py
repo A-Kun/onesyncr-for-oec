34cc055258dc67a300de7b8bc4d2736b4c407a84
Andrew Wang <andrewpy.wang@mail.utoronto.ca>
2016-11-08 04:07:38 -0500

    Add frontend

--- src/service.py
+++ src/service.py
@@ -1,11 +1,43 @@
 #!flask/bin/python
 from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import abort
+import github3
+from github3 import login
+import requests
+import re
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
-@app.route('/')
-def index():
+file = open("static/dashboard.html")
+DASHBOARD = file.read()
+file.close()
+del file
+
+
+@app.route("/api", methods=["GET"])
+def hello_world():
     return "Hello, World!"
 
-if __name__ == '__main__':
+
+@app.route("/", methods=["GET"])
+def login_page():
+    return app.send_static_file("login.html")
+
+
+@app.route("/dashboard", methods=["GET"])
+def login_github():
+    code = request.args.get("code")
+    response = requests.post("https://github.com/login/oauth/access_token?client_id=56e49d113b3037c709a7"
+                             + "&client_secret=aa2f85d97a29ad2c4791af0abd869a24a45eb970&code=" + code)
+    content = response.content.decode()
+    match = re.match("access_token=(.+?)&|$", content)
+    token = match.group(1)
+    gh = login(token=token)
+    user = gh.user()
+    return DASHBOARD.replace("{{ name }}", user.name).replace("{{ login }}", user.login)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
