506f5fcba88bef77c76044c5c7e9df2237695f5b
Andrew Wang <andrewpy.wang@mail.utoronto.ca>
2016-11-10 09:54:04 -0500

    Separate redirect and dashboard pages

--- src/service.py
+++ src/service.py
@@ -10,10 +10,14 @@ import re
 
 app = Flask(__name__, static_url_path='/static')
 
-file = open("static/dashboard.html")
-DASHBOARD = file.read()
-file.close()
-del file
+redirect_file = open("static/redirect.html")
+REDIRECT_PAGE = redirect_file.read()
+redirect_file.close()
+del redirect_file
+dashboard_file = open("static/dashboard.html")
+DASHBOARD_PAGE = dashboard_file.read()
+dashboard_file.close()
+del dashboard_file
 
 
 @app.route("/api", methods=["GET"])
@@ -26,17 +30,25 @@ def login_page():
     return app.send_static_file("login.html")
 
 
-@app.route("/dashboard", methods=["GET"])
-def login_github():
+@app.route("/redirect", methods=["GET"])
+def login_redirect():
     code = request.args.get("code")
-    response = requests.post("https://github.com/login/oauth/access_token?client_id=56e49d113b3037c709a7"
-                             + "&client_secret=aa2f85d97a29ad2c4791af0abd869a24a45eb970&code=" + code)
+    response = requests.post("https://github.com/login/oauth/access_token?client_id=56e49d113b3037c709a7" +
+                             "&client_secret=aa2f85d97a29ad2c4791af0abd869a24a45eb970&code=" + code)
     content = response.content.decode()
     match = re.match("access_token=(.+?)&|$", content)
     token = match.group(1)
     gh = login(token=token)
     user = gh.user()
-    return DASHBOARD.replace("{{ name }}", user.name).replace("{{ login }}", user.login)
+    return REDIRECT_PAGE.replace("{{ name }}", user.name).replace("{{ login }}", user.login).replace("{{ token }}", token)
+
+
+@app.route("/dashboard", methods=["GET"])
+def dashboard():
+    token = request.args.get("token")
+    gh = login(token=token)
+    user = gh.user()
+    return DASHBOARD_PAGE.replace("{{ name }}", user.name).replace("{{ login }}", user.login).replace("{{ token }}", token)
 
 
 if __name__ == "__main__":
